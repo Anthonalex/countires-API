c22f8f548e8485428d9cd22103a214f8dc655af2
Anthonalex <Anthonalex@users.noreply.github.com>
2021-04-07 00:53:02 +0400

    string validation done for search input

--- main-page/main-page.js
+++ main-page/main-page.js
@@ -1,12 +1,26 @@
 const logOutBtn = document.querySelector(".logout-btn");
 const searchInput = document.querySelector(".search-input");
+const validationAlert = document.querySelector(".validation-alert");
 
 searchInput.value = localStorage.getItem("value");
+
+const isValidInput = (data) => {
+  if (isNaN(+data) || data === " " || data === null) {
+    return true;
+  }
+  return false;
+};
 
 logOutBtn.addEventListener("click", () => {
   window.location.href = "/index.html";
 });
 
-searchInput.addEventListener("input", () => {
-  localStorage.setItem("value", `${searchInput.value}`);
+searchInput.addEventListener("input", (event) => {
+  if (isValidInput(event.data)) {
+    validationAlert.classList.remove("active");
+    localStorage.setItem("value", `${searchInput.value}`);
+  } else {
+    validationAlert.classList.add("active");
+    searchInput.value = localStorage.getItem("value");
+  }
 });
