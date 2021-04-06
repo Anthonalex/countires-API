89a7510b39a957b19d892ec1efa69407b9b3edeb
Anthonalex <Anthonalex@users.noreply.github.com>
2021-04-06 22:35:47 +0400

    local storage seted from search input

--- main-page/main-page.js
+++ main-page/main-page.js
@@ -1,6 +1,12 @@
 const logOutBtn = document.querySelector(".logout-btn");
+const searchInput = document.querySelector(".search-input");
+
+searchInput.value = localStorage.getItem("value");
 
 logOutBtn.addEventListener("click", () => {
   window.location.href = "/index.html";
 });
 
+searchInput.addEventListener("input", () => {
+  localStorage.setItem("value", `${searchInput.value}`);
+});
