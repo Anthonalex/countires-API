e431c2de007029cdad4289ae4b471ec8d896440b
Anthonalex <Anthonalex@users.noreply.github.com>
2021-04-07 21:47:11 +0400

    favorites tab done,add and remove btn works well,project done

--- favorites-page/favorites.js
+++ favorites-page/favorites.js
@@ -20,9 +20,19 @@ const renderFavorites = (storage) => {
 };
 
 window.addEventListener("click", (event) => {
-  event.path[0].classList.remove("added");
-  event.path[0].textContent = "+";
-  localStorage.removeItem(`${event.path[0].id}`);
+  if (event.path[0].textContent === "-") {
+    event.path[0].classList.remove("added");
+    event.path[0].textContent = "+";
+    localStorage.removeItem(`${event.path[0].id}`);
+  } else if (event.path[0].textContent === "+") {
+    event.path[0].classList.add("added");
+    event.path[0].textContent = "-";
+
+    localStorage.setItem(
+      `${event.path[0].id}`,
+      `${JSON.stringify(event.path[1].innerHTML)}`
+    );
+  }
 });
 
 renderFavorites();
